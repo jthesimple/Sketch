82ab8049da8402beb63854834abe72f96a79ff04
Julio <julio.bermudez619@gmail.com>
2023-02-08 15:12:18 -0800

    Finalized grid and reset functions.

--- README.md
+++ README.md
@@ -10,4 +10,8 @@ I was able to create the panels with CSS grid, after messing around with flexbox
 
 I was able to add the event listener to the panels. However, the event listener was attached to the grid container, so when you hovered over the container all panels changed color. I tried to refactor my code, but I broke it. The event listener no longer works. 
 
+2/8
 
+I placed the event listener inside the loop as the divs were being created. Finalized the reset button.
+
+My only concern is that I initially create 10k divs tags and only utilize 256 divs until the user request a larger grid. When the user request a larger grid more of the divs populate with the event listeners. 
--- sketch.js
+++ sketch.js
@@ -1,50 +1,49 @@
-/// First function will create a grid of 16x16 square div tags.
-
 const gridContainer = document.createElement('div');
 gridContainer.classList.add('container');
 body = document.querySelector('body');
 body.appendChild(gridContainer);
+gridSize = '';
+
+
 
 createGrid();
 restartSketch();
 
 
+
+/// First function will create 100x100 div tags. Our CSS file will create a 16x16 grid. 
+
 function createGrid(){
     let i = 0;
     do {
-        i += 1;
+        i++ ; 
         let newDivs = document.createElement('div');
         newDivs.classList.add('panel');
         gridContainer.appendChild(newDivs);
-    } while (i <=15);
-
+        newDivs.addEventListener('mouseover', (event)=>{
+            event.target.style['background-color']='green';
+        })
+    } while (i <= 10000);   
 }
 
+/// Function below creates reset button and subsequently ask the user for the new size of the grid.
 
-///Create reset button
 
 function restartSketch(){
     const btn = document.createElement('button');
     btn.textContent ='Restart Sketch Book';
     body.insertBefore(btn,gridContainer);
-
-    restart = document.querySelector('button');
-    restart.addEventListener('click', (event)=> {
-        gridSize = prompt('How big should the grid be?');
-    })
-
+    
+    btn.addEventListener('click', (event)=> {
+        gridSize = prompt('How many panels on each side?');
+        if (gridSize > 100){
+            window.alert('Grid size is too big ! Max size is 100x100. Try Again !')
+        } else {
+        panels = document.querySelectorAll('.panel');
+        panels.forEach(panel=> panel.style['background-color']='');
+        gridContainer.setAttribute('style', `grid-template-rows:repeat(${gridSize}, 1fr); grid-template-columns:repeat(${gridSize}, 1fr);`);
+        }
+    })   
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
